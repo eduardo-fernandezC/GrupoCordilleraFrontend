f51476ab82ca3caa218ccad2276192405ac9a362
Eduardo Fernandez <ed.fernandezc@duocuc.cl>
2026-04-29 13:59:14 -0400

    feat: implemtacion cerrar sesion

---
+++ src/components/molecules/LogoutButton.jsx
@@ -0,0 +1,18 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import Button from "../atoms/Button";
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  const handleLogout = () => {
+    logout({
+      logoutParams: {
+        returnTo: window.location.origin,
+      },
+    });
+  };
+
+  return <Button text="cerrar sesion" onClick={handleLogout} className="" />;
+};
+
+export default LogoutButton;
--- src/pages/Analyst.jsx
+++ src/pages/Analyst.jsx
@@ -1,9 +1,11 @@
+import LogoutButton from "../components/molecules/LogoutButton";
 import LandingTemplate from "../components/templates/LandingTemplate";
 
 const Analyst = () => {
   return (
     <LandingTemplate>
       <h1>Analista</h1>
+      <LogoutButton />
     </LandingTemplate>
   );
 };
