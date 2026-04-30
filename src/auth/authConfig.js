f07c08f6c9185453c540c8fb9b2518f6ec7d7094
Eduardo Fernandez <ed.fernandezc@duocuc.cl>
2026-04-29 22:46:46 -0400

    feat: implementacion de boton de logout en navbar

--- src/auth/authConfig.js
+++ src/auth/authConfig.js
@@ -2,5 +2,5 @@ export const auth0Config = {
   domain: "dev-bphtbl2abtrdlqe1.us.auth0.com",
   clientId: "VckYDqR13oIpTrOAxvkCbYZCWi6wJuxw",
   audience: "https://grupo-cordillera-api",
-  scope: "openid profile email ",
+  scope: "openid profile email",
 };
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,11 +1,9 @@
-import LogoutButton from "../components/molecules/LogoutButton";
 import LandingTemplate from "../components/templates/LandingTemplate";
 
 const Admin = () => {
   return (
     <LandingTemplate>
       <h1>Admin</h1>
-      <LogoutButton />
     </LandingTemplate>
   );
 };
