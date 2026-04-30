9b52e0907fe5c1ec4485e4541af5bf7faece1cf1
Eduardo Fernandez <ed.fernandezc@duocuc.cl>
2026-04-29 23:29:12 -0400

    feat: implemtacion de css a la pagina NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,20 @@
+import "../styles/pages/NotFound.css";
+
 const NotFound = () => {
   return (
-    <>
-      <div>
+    <main className="not-found-page">
+      <div className="not-found-page__content">
+        <h1 className="hero-section__title">404</h1>
         <img
+          className="not-found-page__image"
           src="https://media4.giphy.com/media/v1.Y2lkPTZjMDliOTUyOXZ1dnlobGF3N3JtM3h3YWNlbHF5M21sdm5td2pxbzEyaGU3OHBsMSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/NTur7XlVDUdqM/source.gif"
           alt="404"
         />
+        <a className="not-found-page__link" href="/">
+          🡸 Volver
+        </a>
       </div>
-      <a href="/">🡸 Volver</a>
-    </>
+    </main>
   );
 };
 
