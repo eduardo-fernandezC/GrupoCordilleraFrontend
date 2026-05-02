60964569a171dc38ee2bd5ef145b39990faa35fc
Eduardo Fernandez <ed.fernandezc@duocuc.cl>
2026-05-02 19:21:18 -0400

    feat: implementacion de Loader

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate } from "react-router-dom";
 import { getRoles } from "../auth/Roles";
+import Loader from "../components/atoms/Loader";
 
 const ProtectedRoute = ({ children, role }) => {
   const { isAuthenticated, user, isLoading } = useAuth0();
 
   if (isLoading) {
-    return <h1>Cargando...</h1>;
+    return <Loader />;
   }
 
   if (!isAuthenticated) {
