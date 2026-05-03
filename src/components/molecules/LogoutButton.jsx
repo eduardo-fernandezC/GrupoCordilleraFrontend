4ec889b41f0de7d717e3c8362945635231f4fab3
Eduardo Fernandez <ed.fernandezc@duocuc.cl>
2026-05-03 13:19:43 -0400

    feat: implementacion de advertencia de cerrar sesion

--- src/components/molecules/LogoutButton.jsx
+++ src/components/molecules/LogoutButton.jsx
@@ -1,14 +1,25 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from "../atoms/Button";
+import { getRoles } from "../../auth/Roles";
 
 const LogoutButton = () => {
-  const { logout } = useAuth0();
+  const { logout, user } = useAuth0();
+
+  const roles = getRoles(user);
+  const hasAssignedRole = roles.includes("ADMIN") || roles.includes("ANALISTA");
 
   const handleLogout = () => {
+    if (hasAssignedRole) {
+      const confirmed = window.confirm(
+        "¿Estas seguro de que deseas cerra sesion?",
+      );
+      if (!confirmed) return;
+    }
     logout({
       logoutParams: {
         returnTo: window.location.origin,
       },
+      federated: true,
     });
   };
 
