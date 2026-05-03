655070fde863ddca6b28b2a1b7a4f78b27195eab
Eduardo Fernandez <ed.fernandezc@duocuc.cl>
2026-05-03 14:16:05 -0400

    fix: quitar console.logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import AppRoutes from "./app/AppRoutes";
 
 function App() {
-  const { user } = useAuth0();
-
-  const role = user?.["https://grupo-cordillera-api/roles"] || user?.role;
-
-  console.log(user);
-  console.log(role);
   return <AppRoutes />;
 }
 
