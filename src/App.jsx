ccc7f133690d489aff4e35fc2bfa1bb276e85770
Rocio Mino <roc.mino@duocuc.cl>
2026-05-08 18:44:53 -0400

    feat: dockerizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import AppRoutes from "./app/AppRoutes";
 import NotificationContainer from "./components/atoms/Notification";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
   return (
